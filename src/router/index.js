a9a1ab384f608e9ada742b52c16ea6b245c92494
aria <15608176098@163.com>
2019-03-28 14:15:24 +0800

    sss

--- src/router/index.js
+++ src/router/index.js
@@ -145,7 +145,7 @@ export const constantRouterMap = [
 ]
 
 export default new Router({
-  // mode: 'history', // require service support
+  mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
